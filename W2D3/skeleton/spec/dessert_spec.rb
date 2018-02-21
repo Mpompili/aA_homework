d99dcb4ccda7ca7fc4e7f554f7d882eb9036902f
Michael Pompili <michael@churroboros.com>
2018-02-21 08:38:46 -0800

    finished it: w2d3 hw

--- W2D3/skeleton/spec/dessert_spec.rb
+++ W2D3/skeleton/spec/dessert_spec.rb
@@ -49,16 +49,28 @@ describe Dessert do
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    it "subtracts an amount from the quantity" do
+      brownie.eat(20)
+      expect(brownie.quantity).to eq(80)
+    end
 
-    it "raises an error if the amount is greater than the quantity"
+    it "raises an error if the amount is greater than the quantity" do
+    expect{ brownie.eat(120)}.to raise_error("not enough left!")
+    end
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do
+      allow(chef).to receive(:titleize).and_return("Chef Tucker the Great Baker")
+      expect(brownie.serve).to eq("Chef Tucker the Great Baker has made 100 brownies!")
+    end
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake).with(brownie)
+      brownie.make_more
+    end
   end
+
 end
