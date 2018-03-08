d2556a0c17145626012ceed0266184db96824924
Michael Pompili <michael@churroboros.com>
2018-03-08 09:23:55 -0800

    mailer added

--- W4D3/app/mailers/user_mailer.rb
+++ W4D3/app/mailers/user_mailer.rb
@@ -1,3 +1,9 @@
 class UserMailer < ApplicationMailer
-  default from: 'from@exmaple.com'
-end 
+  default from: 'from@example.com'
+
+  def welcome_email(user)
+    @user = user
+    @url = 'localhost:3000/session/new'
+    mail(to: @user, subject: 'Thanks for signing up!')
+  end
+end
